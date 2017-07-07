d94dec0146223a664dbd8d4b67c9e790add0d3e4
AliceYunanTang <tangyunan@hotmail.com>
2017-07-07 16:39:58 +1000

    initial environment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+
+  before_action :get_user,       only: [ :show, :edit, :update ]
+  before_action :check_if_admin, only: [ :index ]
+
+  before_action :check_if_logged_in, only: [ :mixtape_create ]
+
+  def get_user
+    @user = User.find params["id"]
+  end
+
+  def new
+    @user = User.new
+    # session["rando"] = Random.rand 100000
+  end
+
+  def create
+    @user = User.create user_params
+    if @user.id.present?
+      session[:user_id] = @user.id  # perform login (set session)
+      redirect_to user_path(@user.id)   # /users/17
+    else
+      render :new
+    end
+
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    # catches URLS like /users/:id
+    # @user = User.find params["id"]   # now in before_action
+  end
+
+  def edit
+    # @user = User.find params["id"]   # now in before_action
+    redirect_to root_path unless @current_user == @user
+  end
+
+  def update
+    # @user = User.find params["id"]   # now in before_action
+    # redirect_to root_path unless @current_user == @user
+
+    @user = @current_user # makes sure user can only edit their own profile
+
+    @user.update user_params
+    redirect_to user_path( params["id"] )
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :image, :password, :password_confirmation)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+u1 = User.create email: 'politisj@gmail.com' , name: 'John Politis' , password: 'chicken', is_admin:true
+u2 = User.create email: 'zin@gmail.com' , name: 'Zin' , password: 'chicken', is_admin:false
+u3 = User.create email: 'anusha@gmail.com' , name: 'Anusha' , password: 'chicken', is_admin:true
+u4 = User.create email: 'alice@gmail.com' , name: 'Alice Tang' , password: 'chicken', is_admin:true
+
+# u1 = User.create email: 'politisj@gmail.com', name: 'John Politis' ,password_digest: 'chicken', is_admin:true
+# u2 = User.create email: 'fred@gmail.com', name: 'fred freddddd' ,password_digest: 'chicken', is_admin:false
+# u3 = User.create email: 'blob@gmail.com', name: 'blob blobblob' ,password_digest: 'chicken', is_admin:false
+# u4 = User.create email: 'bill@gmail.com', name: 'bill billbill' ,password_digest: 'chicken', is_admin: true
