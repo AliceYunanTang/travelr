8604bbc3982c4503980f435e007b2d8308da6a4b
AliceYunanTang <tangyunan@hotmail.com>
2017-07-11 15:43:42 +1000

    spots crud done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   def show
+    @comment = Comment.find params["id"]
   end
 
   def index
@@ -7,15 +8,30 @@ class CommentsController < ApplicationController
   end
 
   def create
-      desc = params["description"]
-      spot_id = params["spot_id"]
-      Comment.create description: desc , spot_id: spot_id
-      redirect_to comments_path
+      @comment = Comment.new comment_params
+      # desc = params["description"]
+      # spot_id = params["spot_id"]
+      if params[:file].present?
+        req = Cloudinary::Uploader.upload(params[:file])
+        @comment.image = req['public_id']
+      else
+        @comment.image = "v1499518624/sljlppoeqzc8tmcwylrq.jpg"
+      end
+
+      @comment.spot_id = params["spot_id"]
+
+      if @comment.save
+        redirect_to comments_path
+      end
+
+      # Comment.create description: desc , spot_id: spot_id
+      # Comment.create comment_params
+      # redirect_to comments_path
   end
 
   private
-  def user_params
-    params.require(Comment).permit(:description , :spot_id)
+  def comment_params
+    params.require(:comment).permit(:description , :spot_id)
   end
 
 end
